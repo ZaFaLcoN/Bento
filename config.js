f23eb1ff768c6d42a236c6b5d25a618d95dc6aea
ZaFaLcoN <72493919+ZaFaLcoN@users.noreply.github.com>
2023-04-02 18:05:58 +0300

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'xion',
+	name: 'Xiontora',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
@@ -32,8 +32,8 @@ const CONFIG = {
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '40.8028
+	defaultLongitude: '29.4307
 
 	// Autochange
 	autoChangeTheme: true,
